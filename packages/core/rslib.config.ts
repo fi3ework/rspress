3d29f12ea0546e63778f88863839921fb8a6f3a5
fi3ework <fi3ework@gmail.com>
2024-11-25 18:26:56 +0800

    build: migrate @rspress/core to Rslib build

---
+++ packages/core/rslib.config.ts
@@ -0,0 +1,91 @@
+import { defineConfig } from '@rslib/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      dts: {
+        bundle: true,
+      },
+      syntax: 'es2020',
+      shims: {
+        esm: {
+          require: true,
+        },
+      },
+      output: {
+        distPath: {
+          root: './dist',
+        },
+        externals: [
+          {
+            'react-syntax-highlighter/dist/cjs/languages/prism/supported-languages':
+              'commonjs react-syntax-highlighter/dist/cjs/languages/prism/supported-languages',
+          },
+          '@rspress/mdx-rs',
+          'jsdom',
+          'tailwindcss',
+          '@rspress/plugin-container-syntax',
+          '../compiled/globby/index.js',
+        ],
+      },
+    },
+    {
+      format: 'esm',
+      syntax: 'es2020',
+      dts: {
+        bundle: true,
+      },
+      shims: {
+        esm: {
+          require: true,
+        },
+      },
+      output: {
+        distPath: {
+          root: './dist',
+        },
+        externals: {
+          '@rspress/mdx-rs': 'commonjs @rspress/mdx-rs',
+        },
+      },
+      source: {
+        entry: {
+          loader: './src/node/mdx/loader.ts',
+        },
+      },
+    },
+    {
+      bundle: false,
+      dts: true,
+      format: 'esm',
+      syntax: 'es2020',
+      source: {
+        entry: {
+          index: './src/runtime/*',
+        },
+        tsconfigPath: './src/runtime/tsconfig.json',
+      },
+      output: {
+        externals: [
+          ({ request, dependencyType }, callback) => {
+            if (/^(virtual-routes-ssr|virtual-routes)$/.test(request)) {
+              // Externalize to a commonjs module using the request path
+              return callback(
+                null,
+                `${dependencyType === 'commonjs' ? 'commonjs' : 'module-import'} ${request}`,
+              );
+            }
+
+            callback();
+          },
+        ],
+        distPath: {
+          root: './dist/runtime',
+        },
+      },
+      plugins: [pluginReact()],
+    },
+  ],
+});
